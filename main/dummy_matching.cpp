81338b154657cade4b29e3d86288641653e35756
bob-dubai <bobby@uney.com>
2024-10-04 11:19:29 +0400

    modified to test performance

--- main/dummy_matching.cpp
+++ main/dummy_matching.cpp
@@ -10,17 +10,41 @@
 
 using namespace LOB;
 
-int main() {
-    LimitOrderBook book;
-    std::default_random_engine generator;
-    auto price = std::normal_distribution<double>(500, 20);
-    auto quantity = std::normal_distribution<double>(1000, 100);
-    for (int i = 1; i < 10000000; i++) {
+
+inline void spam_limit_many_market_orders(
+    LimitOrderBook& book,
+    int count,
+    int price_mean = 60000,
+    int price_variance = 1000,
+    int quantity_mean = 1000,
+    int quantity_variance = 10
+) {
+    auto generator = std::default_random_engine();
+    auto price = std::normal_distribution<double>(price_mean, price_variance);
+    auto quantity = std::normal_distribution<double>(quantity_mean, quantity_variance);
+    for (int i = 1; i < count; i++) {
         auto price_ = static_cast<uint64_t>(price(generator));
         auto quantity_ = static_cast<uint32_t>(quantity(generator));
-        book.limit(Side::Buy, 0, 100, price_);
-        if (i % 11 == 0) {  // randomly submit a market order
-            book.market(Side::Sell, 1, quantity_);
-        }
+        // std::cout << "Limit Order: " << i << " Price: " << price_ << " Quantity: " << quantity_ << std::endl;
+        book.limit(Side::Buy, i, 100, price_);
+        book.market(Side::Sell, i, quantity_);
     }
+}
+
+int main() {
+    int count = 5000000;
+  
+    // Start timing
+    auto start = std::chrono::high_resolution_clock::now();
+    auto book = LimitOrderBook();
+    spam_limit_many_market_orders(book, count);
+
+    // End timing
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+
+    std::cout << "Total Orders " << count << " have been submitted." << std::endl;
+    std::cout << "Time taken: " << elapsed.count() << " seconds." << std::endl;
+
+    return 0;
 }
